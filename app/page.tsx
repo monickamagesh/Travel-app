9ef741fdc34a1c9774f6311d69347bb843ea64e6
monickamagesh <monickamagesh@gmail.com>
2024-08-12 10:22:08 +0530

    Components setup updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="bg-gray-50 text-right">
-      hello
+    <main className="">
+      <Hero />
+      <Camp />
+      <Features />
+      <Guide />
+      <GetApp />
     </main>
   );
 }
